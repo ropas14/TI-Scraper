d272f788cf63927b46dff6027a4b2fd8748067ad
ropas14 <37495035+ropas14@users.noreply.github.com>
2018-11-02 15:06:42 +0200

    add

--- readpdf.js
+++ readpdf.js
@@ -2,16 +2,39 @@
    const pdf = require('pdf-parse');
 function readpdfile(path){
   let dataBuffer = fs.readFileSync(path);
-        pdf(dataBuffer).then(function(data) {
-            console.log(data.text);
+        pdf(dataBuffer).then(function(data) {        
             let pdftext = data.text;
-            if(pdftext.indexOf('ACTIVE')>-1){
-                    console.log(pdftext.indexOf('ACTIVE'));
+            let arrDevices = [];
+            if(pdftext.indexOf('Samples')>-1){
+                // remove upper unneccessary text from pdf content
+               pdftext= pdftext.substring((pdftext.indexOf('Samples')+"Samples".length), pdftext.length);
+                //remove bottom contents not needed
+                pdftext = pdftext.split('(1)')[0].trim();
+                // split each line details with a comma instead of newline
+                pdftext=pdftext.replace(/\n/g,",");
+                // now put each row in an array by splitting with a comma
+                pdftext=pdftext.split(',');
+
+                pdftext.forEach(function(item){
+                    if(item.indexOf('ACTIVE')>-1){
+                        item=item.split('ACTIVE')[0];
+                        //console.log(item);
+                        arrDevices.push(item);
+                    }
+                    else if (item.indexOf('PREVIEW')>-1){
+                          item=item.split('PREVIEW')[0];
+                         // console.log(item);
+                          arrDevices.push(item);
+                    }
+                    else{}
+                });
+               // console.log(arrDevices);
+                return arrDevices;
             }
-            else{
-                console.log(pdftext.indexOf('PREVIEW'));
-            }
+            
         });
     }
 
     module.exports=readpdfile;
+
+  // readpdfile('./pdfs/am5746_oa.pdf');
